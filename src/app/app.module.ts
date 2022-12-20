9345e6e43caac94d45f45fe02936b1889ca135eb
malwozniak <mgrztwnk5@gmail.com>
2022-12-20 08:58:54 +0100

    4 files modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ArchiveComponent } from './components/archive/archive.component';
+import { MatMomentDateModule } from '@angular/material-moment-adapter';
 
 @NgModule({
   imports: [
@@ -32,6 +33,7 @@ import { ArchiveComponent } from './components/archive/archive.component';
     MatExpansionModule,
     MatIconModule,
     MatSnackBarModule,
+    MatMomentDateModule,
     MatSlideToggleModule,
     MatChipsModule,
     MatToolbarModule,
